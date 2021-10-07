66549b158f5b1753f0469efc22a453d3c88d82f4
unknown <you@example.com>
2021-10-07 09:25:03 +0300

    password

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPassword
+{
+    public class PasswordChecker
+    {
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#@$%^&_").Count() == 0)
+                return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            bool res = validatePassword("ASDqwe123$");
+            Console.WriteLine("res " + res);
+        }
+    }
+}
